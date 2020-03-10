7488051f63e8a90f971322b7e000463305cbe801
CbFernando <carlos.santos@retornar.org>
2020-03-10 16:01:07 -0300

    defining SSO information in AddIdentityServerAuthentication and specifying the API that uses the bearer's token in calls in AddAuthentication

--- SecureApi/Startup.cs
+++ SecureApi/Startup.cs
@@ -1,30 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IdentityModel;
+using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace SecureApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Setting OAuth2 configuration
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            services.AddAuthentication(o =>
+                {
+                    o.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                    o.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://jpproject-sso:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiSecret = "api-secret";
+                    options.ApiName = "report-api";
+                    options.RoleClaimType = JwtClaimTypes.Role;
+                    options.NameClaimType = JwtClaimTypes.Name;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +42,8 @@ namespace SecureApi
 
             app.UseRouting();
 
+            // Adding auth and auth for api
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
